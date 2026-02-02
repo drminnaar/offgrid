be810d4cb948b7aa517e87ad9deed5219d89b9cb
Douglas Minnaar <33935506+drminnaar@users.noreply.github.com>
2026-02-02 20:21:00 +1300

    feat(shop/app): Add navigation (home) on brand click

--- apps/shop/shop-app/src/components/ui/layout/app-bar-brand.tsx
+++ apps/shop/shop-app/src/components/ui/layout/app-bar-brand.tsx
@@ -2,14 +2,17 @@
 
 import { NavbarBrand } from '@heroui/react';
 import { MountainSnow } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 export type AppTopBarBrandProps = {
   className?: string;
 };
 
 export const AppBarBrand = ({ className }: AppTopBarBrandProps) => {
+  const router = useRouter();
+  const classNames = className + ' cursor-pointer';
   return (
-    <NavbarBrand className={className}>
+    <NavbarBrand className={classNames} onClick={() => router.push('/')}>
       <MountainSnow size={32} color='#338cf1' />
       <p className='font-bold text-inherit' style={{ color: '#338cf1' }}>
         OFFGRID
