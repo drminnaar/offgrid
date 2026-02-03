d269390b5282c798cee73cd5c46a5f399d65354d
Douglas Minnaar <33935506+drminnaar@users.noreply.github.com>
2026-02-03 17:32:40 +1300

    feat(portal/app): Add initial portal application
    - Add favicons
    - Install Roboto font
    - Update README

---
+++ apps/portal/portal-app/src/App.tsx
@@ -0,0 +1,10 @@
+// styles
+import './App.css';
+
+export const App = () => {
+  return (
+    <>
+      <div>Offgrid Portal</div>
+    </>
+  );
+};
---
+++ apps/portal/portal-app/src/main.tsx
@@ -0,0 +1,15 @@
+// packages
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+// styles
+import './index.css';
+
+// components
+import { App } from './App';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
