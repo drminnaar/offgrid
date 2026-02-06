6b5c209f8a66e67c3ac7b5142cc9b5e6fe1a6cd6
Douglas Minnaar <33935506+drminnaar@users.noreply.github.com>
2026-02-06 21:53:47 +1300

    feat(lib/dotnet): Add validation error abstraction

--- apps/libs/dotnet/src/Offgrid.Framework/Exceptions/ValidationException.cs
+++ apps/libs/dotnet/src/Offgrid.Framework/Exceptions/ValidationException.cs
@@ -1,4 +1,6 @@
-﻿namespace Offgrid.Framework.Exceptions;
+﻿using Offgrid.Framework.System;
+
+namespace Offgrid.Framework.Exceptions;
 
 [Serializable]
 public sealed class ValidationException : Exception
@@ -10,6 +12,11 @@ public sealed class ValidationException : Exception
         Errors = new Dictionary<string, List<string>>().AsReadOnly();
     }
 
+    public ValidationException(string message, params ValidationError[] errors) : base(message)
+    {
+        Errors = errors.ToDictionary(e => e.Property, e => e.Messages).AsReadOnly();
+    }
+
     public ValidationException(string message, IReadOnlyDictionary<string, List<string>> errors) : base(message)
     {
         Errors = errors;
@@ -18,5 +25,21 @@ public sealed class ValidationException : Exception
     public ValidationException(string message, params KeyValuePair<string, List<string>>[] errors) : base(message)
     {
         Errors = errors.ToDictionary(e => e.Key, e => e.Value).AsReadOnly();
+    }
+
+    public sealed record ValidationError
+    {
+        public string Property { get; }
+        public List<string> Messages { get; }
+
+        public ValidationError(string property, params string[] messages) : this(property, messages.ToList())
+        {
+        }
+
+        public ValidationError(string property, List<string> messages)
+        {
+            Property = property.ToCamelCase();
+            Messages = messages;
+        }
     }
 }
