d2119527d79365595032a7c70363f5cf851788af
Douglas Minnaar <33935506+drminnaar@users.noreply.github.com>
2026-02-08 15:13:15 +1300

    feat(lib/dotnet): Add annotation to validate enumeration

---
+++ apps/libs/dotnet/src/Offgrid.Framework/System.ComponentModel.DataAnnotations/ValidEnumAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Offgrid.Framework.System.ComponentModel.DataAnnotations;
+
+public sealed class ValidEnumAttribute : ValidationAttribute
+{
+    private readonly Type _enumType;
+
+    public ValidEnumAttribute(Type enumType)
+    {
+        _enumType = enumType;
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value == null)
+            return ValidationResult.Success;
+
+        var stringValue = value.ToString();
+
+        if (string.IsNullOrWhiteSpace(stringValue))
+            return new ValidationResult($"The {validationContext.DisplayName} field is required.");
+
+        var enumValues = Enum.GetNames(_enumType);
+
+        if (Enum.TryParse(_enumType, stringValue, ignoreCase: true, out var result) &&
+            Enum.IsDefined(_enumType, result))
+        {
+            return ValidationResult.Success;
+        }
+
+        return new ValidationResult(
+            $"Invalid value for {validationContext.DisplayName}. Valid values are: {string.Join(", ", enumValues)}");
+    }
+}
