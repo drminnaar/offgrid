cf36425b3e7e15a128d18ab0c902351f769442a1
Douglas Minnaar <33935506+drminnaar@users.noreply.github.com>
2026-02-08 15:06:55 +1300

    feat(lib/dotnet): Add extension method to http context to extract username

---
+++ apps/libs/dotnet/src/Offgrid.Framework/AspNetCore/Http/Extensions/Username.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Offgrid.Framework.AspNetCore.Http.Extensions;
+
+public static partial class HttpExtensions
+{
+    public static string Username(this HttpContext httpContext)
+    {
+        return httpContext.User?.Identity?.Name ?? string.Empty;
+    }
+}
