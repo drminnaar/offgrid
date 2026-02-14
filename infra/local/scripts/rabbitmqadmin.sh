455295402ee3a474056af745034823ff5f69f037
Douglas Minnaar <33935506+drminnaar@users.noreply.github.com>
2026-02-14 15:49:19 +1300

    feat(infra/local): Add rabbitmq service
    - Add rabbitmq compose file with healthcheck
    - Add script to launch rabbitmqadmin (with credentials set) and alias
    - Update .env.example with rabbitmq environment variables
    - Update root compose file to include rabbitmq

---
+++ infra/local/scripts/rabbitmqadmin.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+COMPOSE_FILE="$DIR/../compose.yaml"
+
+echo -e "\n"
+
+docker compose --file "$COMPOSE_FILE" --env-file "$DIR/.env" exec rabbitmq sh -c \
+  'printf "export RABBITMQ_USER=$RABBITMQ_DEFAULT_USER\nexport RABBITMQ_PASSWORD=$RABBITMQ_DEFAULT_PASS\nalias ra=\"rabbitmqadmin -u admin -p password\"\n" > /tmp/radminrc && exec bash --rcfile /tmp/radminrc -i'
+  
+
