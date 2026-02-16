54483e66077800a7e429067186a52c1dcc8a85f9
Douglas Minnaar <33935506+drminnaar@users.noreply.github.com>
2026-02-16 22:18:47 +1300

    feat(lib/dotnet): Add enum extensions to convert enum to comma separated value

---
+++ libs/dotnet/src/Offgrid.Framework/System/EnumExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Offgrid.Framework.System;
+
+public static class EnumExtensions
+{
+    public static string ToCommaSeparatedList<TEnum>() where TEnum : Enum
+    {
+        return string.Join(", ", Enum.GetNames(typeof(TEnum)));
+    }
+}
