491bf6a0ffa848c3a353eec33a190e9361c887ae
Douglas Minnaar <33935506+drminnaar@users.noreply.github.com>
2026-02-23 23:08:28 +1300

    feat(lib/dotnet): Add extension to convert datetime to unix time in seconds

---
+++ libs/dotnet/src/Offgrid.Framework/System/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Offgrid.Framework.System;
+
+public static class DateTimeExtensions
+{
+    extension(DateTime source)
+    {
+        public long ToUnixTimeSeconds()
+        {
+            var utcDateTime = source.Kind == DateTimeKind.Utc
+                ? source
+                : source.ToUniversalTime();
+
+            return new DateTimeOffset(utcDateTime).ToUnixTimeSeconds();
+        }
+    }
+}
